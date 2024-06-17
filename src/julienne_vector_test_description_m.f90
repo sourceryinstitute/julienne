141979cb606036fef2f967c602e5b4d802388a95
ktras <krasmussen@lbl.gov>
2024-06-17 17:39:57 +0000

    Deploying to gh-pages from @ sourceryinstitute/julienne@2dbb89e2a639dd3a8e15b731f37d3c5310783dd0 🚀

--- src/julienne_vector_test_description_m.f90
+++ src/julienne_vector_test_description_m.f90
@@ -64,28 +64,4 @@ module julienne_vector_test_description_m
 
   end interface
 
-contains
-
-  module procedure contains_text
-    integer i
-    associate(num_descriptions => size(self%description_vector_))
-      allocate(match_vector(num_descriptions))
-      do i = 1, num_descriptions
-        match_vector(i) = index(self%description_vector_(i)%string(), substring ) /= 0
-      end do
-    end associate
-  end procedure
-
-  module procedure construct
-    vector_test_description%description_vector_ = description_vector
-    vector_test_description%vector_function_strategy_ = vector_function_strategy
-  end procedure
-
-  module procedure run
-    associate(vector_result => self%vector_function_strategy_%vector_function())
-      call assert(size(self%description_vector_)==size(vector_result), "julienne_vector_test_description_s: size match")
-      test_results = test_result_t(self%description_vector_, vector_result)
-    end associate
-  end procedure
-
 end module julienne_vector_test_description_m
