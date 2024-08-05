f39884bc49e3aaa0dfecdbd82497d7d943ad7269
Katherine Rasmussen <krasmussen@lbl.gov>
2024-08-05 15:18:42 -0700

    Update reference to license file in source code

--- src/julienne/julienne_string_s.f90
+++ src/julienne/julienne_string_s.f90
@@ -1,4 +1,4 @@
-! Copyright (c) 2024, Sourcery Institute
+! Copyright (c) 2024, The Regents of the University of California and Sourcery Institute
 ! Terms of use are as specified in LICENSE.txt
 submodule(julienne_string_m) julienne_string_s
   use assert_m, only : assert
--- src/julienne/julienne_test_result_m.f90
+++ src/julienne/julienne_test_result_m.f90
@@ -1,4 +1,4 @@
-! Copyright (c) 2024, Sourcery Institute
+! Copyright (c) 2024, The Regents of the University of California and Sourcery Institute
 ! Terms of use are as specified in LICENSE.txt
 module julienne_test_result_m
   !! Define an abstraction for describing test intentions and results
